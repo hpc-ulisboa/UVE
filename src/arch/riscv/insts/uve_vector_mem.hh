0370124f2514f0d3a1e0ee9b1a4afb03cb6e7b05
João Mário <joaomario.rd@hotmail.com>
2019-05-07 02:02:45 +0100

    16c: ...

--- src/arch/riscv/insts/uve_vector_mem.hh
+++ src/arch/riscv/insts/uve_vector_mem.hh
@@ -21,7 +21,9 @@ class UveMemLoad : public RiscvStaticInst
                    uint8_t _VDest, uint8_t _RS1, uint8_t _RS2) : 
           RiscvStaticInst(mnem, _machInst, __opClass),
           VDest(_VDest), RS1(_RS1), RS2(_RS2)
-    {DPRINTF(UVEMem, "UveMemLoad constructor executed: VDest(%d), RS1(%d), RS2(%d)",VDest, RS1, RS2)}
+    {
+      DPRINTF(UVEMem, "UveMemLoad constructor executed: VDest(%d), RS1(%d), RS2(%d)",VDest, RS1, RS2);
+    }
 
     std::string generateDisassembly(Addr pc, const SymbolTable *symtab) const;
 };
@@ -39,7 +41,9 @@ class UveMemStore : public RiscvStaticInst
                    uint8_t _VSrc1, uint8_t _RS1, uint8_t _RS2) :
           RiscvStaticInst(mnem, _machInst, __opClass),
           VSrc1(_VSrc1), RS1(_RS1), RS2(_RS2)
-    {}
+    {
+      DPRINTF(UVEMem, "UveMemStore constructor executed: VSrc1(%d), RS1(%d), RS2(%d)",VSrc1, RS1, RS2);
+    }
 
     std::string generateDisassembly(Addr pc, const SymbolTable *symtab) const;
 };
