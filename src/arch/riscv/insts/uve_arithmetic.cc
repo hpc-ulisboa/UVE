bec9affed4c52b5012da132bd9f8e0733af83693
joaomariord <joaomario.rd@hotmail.com>
2019-06-03 10:16:01 +0100

    Completed Add Sub Mul Div Abs Mac instructions
    Change-Id: Ic8c5d0540d1288e151fe5c5171d13d79cd8ddcb8

---
+++ src/arch/riscv/insts/uve_arithmetic.cc
@@ -0,0 +1,38 @@
+
+#include "arch/riscv/insts/uve_arithmetic.hh"
+
+#include <sstream>
+#include <string>
+
+#include "arch/riscv/utility.hh"
+
+using namespace std;
+
+namespace RiscvISA
+{
+
+std::string
+UveBaseThreeVecPredicated::generateDisassembly(Addr pc,
+    const SymbolTable *symtab)
+const
+{
+    stringstream ss;
+    ss << mnemonic << getFPSignedRepr() << "\t" <<
+    registerName(_destRegIdx[0]) << ", " << registerName(_srcRegIdx[0]) <<
+    ", " << registerName(_srcRegIdx[1]) << ", " << registerName(_srcRegIdx[2]);
+    return ss.str();
+}
+
+std::string
+UveBaseTwoVecPredicated::generateDisassembly(Addr pc,
+    const SymbolTable *symtab)
+const
+{
+    stringstream ss;
+    ss << mnemonic << getFPSignedRepr() << "\t" <<
+    registerName(_destRegIdx[0]) << ", " << registerName(_srcRegIdx[0]) <<
+    ", " << registerName(_srcRegIdx[2]);
+    return ss.str();
+}
+
+}  // namespace RiscvISA
