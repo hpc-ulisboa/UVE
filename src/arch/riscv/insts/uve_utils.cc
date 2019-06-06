978d98e59637796b09a62d7e8b09b08dcdbb1f26
joaomariord <joaomario.rd@hotmail.com>
2019-06-06 16:48:37 +0100

    Misc instructions completed
    Change-Id: I1deff4d4570396c6100520809a51effbae5b969b

---
+++ src/arch/riscv/insts/uve_utils.cc
@@ -0,0 +1,261 @@
+
+#include "arch/riscv/insts/uve_utils.hh"
+
+#include <sstream>
+#include <string>
+
+#include "arch/riscv/utility.hh"
+
+using namespace std;
+
+namespace RiscvISA{
+
+    template <typename Ret>
+    Ret convertFP(float& val){return (Ret) val;}
+    template <typename Ret>
+    Ret convertFP(double& val){return (Ret) val;}
+
+    template <typename Ret>
+    Ret convertUInt(uint8_t& val){return ((Ret&) val) & 0xff;}
+    template <typename Ret>
+    Ret convertUInt(uint16_t& val){return ((Ret&) val) & 0xffff;}
+    template <typename Ret>
+    Ret convertUInt(uint32_t& val){return ((Ret&) val) & 0xffffffff;}
+    template <typename Ret>
+    Ret convertUInt(uint64_t& val){return ((Ret&) val);}
+
+    template <typename Ret>
+    Ret convertInt(int8_t& val){
+        auto aux = ((Ret&) val) & 0xff;
+        if ((aux >> 7) & 1)
+            aux |= ~(0ULL) << 8;
+        return aux;
+    }
+    template <typename Ret>
+    Ret convertInt(int16_t& val){
+        auto aux = ((Ret&) val) & 0xffff;
+        if ((aux >> 15) & 1)
+            aux |= ~(0ULL) << 16;
+        return aux;
+    }
+    template <typename Ret>
+    Ret convertInt(int32_t& val){
+        auto aux = ((Ret&) val) & 0xffffffff;
+        if ((aux >> 31) & 1)
+            aux |= ~(0ULL) << 32;
+        return aux;
+    }
+    template <typename Ret>
+    Ret convertInt(int64_t& val){
+        return ((Ret&) val);
+    }
+
+    void check_equal_src_widths(size_t a, size_t b){
+        //JMTODO: Use assert in the future
+        if (a!=b)
+            DPRINTF(UVEUtils,"Src widths differ.. invalid src code");
+    }
+
+    size_t get_vector_width(ExecContext *xc, uint8_t reg ){
+        return RiscvStaticInst::getUveVecType(xc->tcBase(),
+            reg);
+    }
+
+
+    template <typename Ret>
+    Ret uveMax(Ret val1, Ret val2){
+        return (val1 > val2 ? val1 : val2);
+    }
+
+    template <typename Ret>
+    Ret uveMin(Ret val1, Ret val2){
+        return (val1 < val2 ? val1 : val2);
+    }
+
+
+    template <typename Ret>
+    Ret castBitsToRetType(uint64_t arg){
+        union
+        {
+            Ret src;
+            uint64_t bits;
+        } val;
+        val.bits = arg;
+        return val.src;
+    }
+
+    template <typename T>
+    uint64_t typeCastToBits(T arg)
+    {
+        union
+        {
+            T src;
+            uint64_t bits;
+        } val;
+        val.src = arg;
+        return val.bits;
+    }
+
+
+    // JMNOTE: Template Explicit Initialization
+
+    template
+    float convertFP(float& val);
+    template
+    double convertFP(float& val);
+    template
+    float convertFP(double& val);
+    template
+    double convertFP(double& val);
+
+    template
+    uint8_t convertUInt(uint8_t& val);
+    template
+    uint16_t convertUInt(uint8_t& val);
+    template
+    uint32_t convertUInt(uint8_t& val);
+    template
+    uint64_t convertUInt(uint8_t& val);
+    template
+    uint8_t convertUInt(uint16_t& val);
+    template
+    uint16_t convertUInt(uint16_t& val);
+    template
+    uint32_t convertUInt(uint16_t& val);
+    template
+    uint64_t convertUInt(uint16_t& val);
+    template
+    uint8_t convertUInt(uint32_t& val);
+    template
+    uint16_t convertUInt(uint32_t& val);
+    template
+    uint32_t convertUInt(uint32_t& val);
+    template
+    uint64_t convertUInt(uint32_t& val);
+    template
+    uint8_t convertUInt(uint64_t& val);
+    template
+    uint16_t convertUInt(uint64_t& val);
+    template
+    uint32_t convertUInt(uint64_t& val);
+    template
+    uint64_t convertUInt(uint64_t& val);
+
+    template
+    int8_t convertInt(int8_t& val);
+    template
+    int16_t convertInt(int8_t& val);
+    template
+    int32_t convertInt(int8_t& val);
+    template
+    int64_t convertInt(int8_t& val);
+    template
+    int8_t convertInt(int16_t& val);
+    template
+    int16_t convertInt(int16_t& val);
+    template
+    int32_t convertInt(int16_t& val);
+    template
+    int64_t convertInt(int16_t& val);
+    template
+    int8_t convertInt(int32_t& val);
+    template
+    int16_t convertInt(int32_t& val);
+    template
+    int32_t convertInt(int32_t& val);
+    template
+    int64_t convertInt(int32_t& val);
+    template
+    int8_t convertInt(int64_t& val);
+    template
+    int16_t convertInt(int64_t& val);
+    template
+    int32_t convertInt(int64_t& val);
+    template
+    int64_t convertInt(int64_t& val);
+
+
+    template
+    int8_t uveMax(int8_t val1, int8_t val2);
+    template
+    int16_t uveMax(int16_t val1, int16_t val2);
+    template
+    int32_t uveMax(int32_t val1, int32_t val2);
+    template
+    int64_t uveMax(int64_t val1, int64_t val2);
+    template
+    uint8_t uveMax(uint8_t val1, uint8_t val2);
+    template
+    uint16_t uveMax(uint16_t val1, uint16_t val2);
+    template
+    uint32_t uveMax(uint32_t val1, uint32_t val2);
+    template
+    uint64_t uveMax(uint64_t val1, uint64_t val2);
+    template
+    float uveMax(float val1, float val2);
+    template
+    double uveMax(double val1, double val2);
+
+    template
+    int8_t uveMin(int8_t val1, int8_t val2);
+    template
+    int16_t uveMin(int16_t val1, int16_t val2);
+    template
+    int32_t uveMin(int32_t val1, int32_t val2);
+    template
+    int64_t uveMin(int64_t val1, int64_t val2);
+    template
+    uint8_t uveMin(uint8_t val1, uint8_t val2);
+    template
+    uint16_t uveMin(uint16_t val1, uint16_t val2);
+    template
+    uint32_t uveMin(uint32_t val1, uint32_t val2);
+    template
+    uint64_t uveMin(uint64_t val1, uint64_t val2);
+    template
+    float uveMin(float val1, float val2);
+    template
+    double uveMin(double val1, double val2);
+
+    template
+    uint8_t castBitsToRetType(uint64_t arg);
+    template
+    uint16_t castBitsToRetType(uint64_t arg);
+    template
+    uint32_t castBitsToRetType(uint64_t arg);
+    template
+    uint64_t castBitsToRetType(uint64_t arg);
+    template
+    int8_t castBitsToRetType(uint64_t arg);
+    template
+    int16_t castBitsToRetType(uint64_t arg);
+    template
+    int32_t castBitsToRetType(uint64_t arg);
+    template
+    int64_t castBitsToRetType(uint64_t arg);
+    template
+    float castBitsToRetType(uint64_t arg);
+    template
+    double castBitsToRetType(uint64_t arg);
+
+    template
+    uint64_t typeCastToBits(uint8_t arg);
+    template
+    uint64_t typeCastToBits(uint16_t arg);
+    template
+    uint64_t typeCastToBits(uint32_t arg);
+    template
+    uint64_t typeCastToBits(uint64_t arg);
+    template
+    uint64_t typeCastToBits(int8_t arg);
+    template
+    uint64_t typeCastToBits(int16_t arg);
+    template
+    uint64_t typeCastToBits(int32_t arg);
+    template
+    uint64_t typeCastToBits(int64_t arg);
+    template
+    uint64_t typeCastToBits(float arg);
+    template
+    uint64_t typeCastToBits(double arg);
+} // namespace RiscvISA
---
+++ src/arch/riscv/insts/uve_utils.hh
@@ -0,0 +1,138 @@
+
+#ifndef __ARCH_RISCV_UVE_UTILS_HH__
+#define __ARCH_RISCV_UVE_UTILS_HH__
+
+#include <stdint.h>
+
+#include "arch/riscv/insts/static_inst.hh"
+#include "debug/UVEUtils.hh"
+
+namespace RiscvISA
+{
+    #define MEM_PRINTF(mem,dir,vec,count)  DPRINTF(UVEMem, "______________" \
+                "__________________\\n"); \
+                std::string ss; \
+                for (int i = 0; i < count; i++) { \
+                    ss += "(" + std::to_string(i) + ") M[" + \
+                    std::to_string(mem[i]) + "]" dir "V[" + \
+                    std::to_string(vec[i]) + "] -"; \
+                    if (i % 30 == 29) ss += "\\n"; \
+                } \
+                DPRINTF(UVEMem, (ss+="\\n").c_str() ); \
+                DPRINTF(UVEMem, "________________________________\\n");
+
+    template <typename Ret>
+    Ret convertFP(float& val);
+    template <typename Ret>
+    Ret convertFP(double& val);
+
+    template <typename Ret>
+    Ret convertUInt(uint8_t& val);
+    template <typename Ret>
+    Ret convertUInt(uint16_t& val);
+    template <typename Ret>
+    Ret convertUInt(uint32_t& val);
+    template <typename Ret>
+    Ret convertUInt(uint64_t& val);
+
+    template <typename Ret>
+    Ret convertInt(int8_t& val);
+    template <typename Ret>
+    Ret convertInt(int16_t& val);
+    template <typename Ret>
+    Ret convertInt(int32_t& val);
+    template <typename Ret>
+    Ret convertInt(int64_t& val);
+
+    void check_equal_src_widths(size_t a, size_t b);
+
+    size_t get_vector_width(ExecContext *xc, uint8_t reg );
+
+    template <typename Ret>
+    typename std::enable_if<std::is_integral<Ret>::value, Ret>::type
+    uveAbs(Ret val){
+        return abs(val);
+    }
+
+    template <typename Ret>
+    typename std::enable_if<std::is_floating_point<Ret>::value, Ret>::type
+    uveAbs(Ret val){
+        return fabs(val);
+    }
+
+    template <typename Ret>
+    typename std::enable_if<std::is_integral<Ret>::value, Ret>::type
+    uveInc(Ret val){
+        return val + 1;
+    }
+
+    template <typename Ret>
+    typename std::enable_if<std::is_floating_point<Ret>::value, Ret>::type
+    uveInc(Ret val){
+        return val + 1.0f;
+    }
+
+    template <typename Ret>
+    typename std::enable_if<std::is_integral<Ret>::value, Ret>::type
+    uveDec(Ret val){
+        return val - 1;
+    }
+
+    template <typename Ret>
+    typename std::enable_if<std::is_floating_point<Ret>::value, Ret>::type
+    uveDec(Ret val){
+        return val - 1.0f;
+    }
+
+    template <typename Ret>
+    Ret uveMax(Ret val1, Ret val2);
+
+    template <typename Ret>
+    Ret uveMin(Ret val1, Ret val2);
+
+    template <typename Ret>
+    Ret castBitsToRetType(uint64_t arg);
+
+    template <typename T>
+    uint64_t typeCastToBits(T arg);
+
+
+    // JMNOTE: Template Specialization
+    template <class WidthSize>
+    struct width_converter{
+        static const uint8_t width = 3;
+    };
+    template <>
+    struct width_converter<uint32_t>{
+        static const uint8_t width = 2;
+    };
+    template <>
+    struct width_converter<uint16_t>{
+        static const uint8_t width = 1;
+    };
+    template <>
+    struct width_converter<uint8_t>{
+        static const uint8_t width = 0;
+    };
+    template <>
+    struct width_converter<int32_t>{
+        static const uint8_t width = 2;
+    };
+    template <>
+    struct width_converter<int16_t>{
+        static const uint8_t width = 1;
+    };
+    template <>
+    struct width_converter<int8_t>{
+        static const uint8_t width = 0;
+    };
+    template <>
+    struct width_converter<float>{
+        static const uint8_t width = 2;
+    };
+
+
+} // namespace RiscvISA
+
+
+#endif  // __ARCH_RISCV_UVE_UTILS_HH__
