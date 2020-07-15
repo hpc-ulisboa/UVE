bf6f7eea6aa3f81c4883cd8d1e861e5a941faeac
Joao <joaomario.rd@hotmail.com>
2020-07-15 11:21:24 +0100

    Created ISA interface for stream modifier insts
    Change-Id: I49fda9f5dcff80d95e8daa3ee1e94628dc62454f

--- src/arch/riscv/insts/uve_stream.cc
+++ src/arch/riscv/insts/uve_stream.cc
@@ -23,5 +23,16 @@ const
        << ", " << registerName(_srcRegIdx[2]);
     return ss.str();
 }
+std::string
+UveBaseModifierStream::generateDisassembly(Addr pc,
+    const SymbolTable *symtab)
+const
+{
+    stringstream ss;
+    ss << mnemonic << "\t"
+       << "u" << (uint32_t)this->getStreamRegister() << ", "
+       << registerName(_srcRegIdx[0]) << registerName(_srcRegIdx[1]);
+    return ss.str();
+}
 
 }  // namespace RiscvISA
