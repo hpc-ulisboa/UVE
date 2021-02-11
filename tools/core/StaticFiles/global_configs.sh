5fdfe3fdd96e15e710d54c1a473ed84ec3a6d165
João Mário <joaomario.rd@hotmail.com>
2021-02-11 18:45:27 +0000

    Added configurations

--- tools/core/StaticFiles/global_configs.sh
+++ tools/core/StaticFiles/global_configs.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-#Set an e-mail address for automatic results delivery
-EMAIL=""
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+    DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+GLOBAL_CONFIGS=$DIR/../../../global_configs.sh
+
+echo $DIR
+echo $GLOBAL_CONFIGS
+if test -f "$GLOBAL_CONFIGS"; then
+    echo "File exists"
+    . $GLOBAL_CONFIGS
+fi
