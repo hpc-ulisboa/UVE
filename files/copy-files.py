4a9661437467e4a8ba58e97068858ae9ec5638df
João Mário <joaomario.rd@hotmail.com>
2021-02-23 22:18:33 +0000

    Refactored scripts for relative usage+1

--- files/copy-files.py
+++ files/copy-files.py
@@ -5,14 +5,14 @@ files = ["riscv-dis.c", "riscv-opc.c", "riscv.h", "tc-riscv.c"]
 files_parent = ["opcodes", "opcodes", "opcode", "config"]
 
 if __name__ == "__main__":
-    base_level = Path(".").resolve().parent
-    src_dir = list(base_level.parent.glob("**/riscv-opcodes/files"))[0]
+    base_level = Path(".").resolve()
+    src_dir = list(base_level.glob("./files"))[0]
     
     print("The following files were copied to:")
 
     for file_name, file_parent in zip(files, files_parent):
         src_file = src_dir / file_name
-        dest_file = list(base_level.glob("**/ext_modules/riscv-gnu-toolchain/riscv-binutils/**/{}/{}"
+        dest_file = list(base_level.glob("./ext_modules/riscv-gnu-toolchain/riscv-binutils/**/{}/{}"
             .format(file_parent, file_name)))[0]
         result = shutil.copy(src_file, dest_file)
         print(result)
